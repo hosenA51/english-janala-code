7df22a10d834db5a21f005cb9559a68140516b70
hosenA51 <hosenahmed2552@gmail.com>
2026-02-11 16:51:54 +0600

    added learn vocabulary section and handle errors, empty lessons for better UX

---
+++ script/index.js
@@ -0,0 +1,79 @@
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
+    .then(res=> res.json()) // promise of json data
+    .then(json => displayLesson(json.data));
+};
+
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll("lesson-btn");
+    lessonButtons.forEach(btn=> btn.classList.remove("active"));
+}
+
+const loadLevelWord=(id)=>{
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+        removeActive(); // remove all active class
+        const clickBtn = document.getElementById(`lesson-btn-${id}`);
+        clickBtn.classList.add("active"); // add active class
+        displayLevelWord(data.data)
+    });
+};
+
+const displayLevelWord = (words) => {
+    const wordContainer = document.getElementById("word-container");
+    wordContainer.innerHTML= "";
+
+    if(words.length == 0){
+        wordContainer.innerHTML= `
+        <div class="text-center col-span-full rounded-xl py-10 space-y-6 font-bangla">
+        <img class="mx-auto" src="./assets/alert-error.png"/>
+            <p class="text-xl font-medium text-gray-500">এই Lesson এ এখনো কোন Vocabulary যুক্ত করা হয়নি</p>
+            <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান</h2>
+        </div>
+        `;
+        return;
+    }
+
+    words.forEach(word => {
+        const card = document.createElement("div");
+        card.innerHTML = `
+        <div class="bg-white rounded-xl shadow-sm text-center py-10 px-5 space-y-4">
+            <h2 class="font-bold text-2xl">${word.word ? word.word : "শব্দ পাওয়া যায়নি"}</h2>
+            <p class="font-semibold">Meaning /Pronounciation</p>
+            <div class="text-2xl font-medium font-bangla">"${word.meaning ? word.meaning : "অর্থ পাওয়া যায়নি" } / ${word.pronunciation ? word.pronunciation : "pronunciation পাওয়া যায়নি" }"</div>
+            <div class="flex justify-between items-center">
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-circle-info"></i></button>
+                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80]"><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+        </div>
+        `;
+        wordContainer.append(card);
+    });
+}
+
+const displayLesson = (lessons) => {
+    // 1. get the container & empty
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML="";
+
+    // 2. get into every lessons
+    for(let lesson of lessons){
+        // 3. create Element
+
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML= `
+                <button id="lesson-btn-${lesson.level_no}"
+                onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn">
+                <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
+                </button>
+        `
+        // 4. append into container
+
+        levelContainer.append(btnDiv);
+    }
+    
+};
+
+loadLessons();
