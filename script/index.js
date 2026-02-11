931235ef165a01509b8927d17ef5f039db339385
hosenA51 <hosenahmed2552@gmail.com>
2026-02-11 20:10:59 +0600

    Manage Spinner for loading word data

--- script/index.js
+++ script/index.js
@@ -3,6 +3,16 @@ const createElements = (arr) => {
     return (htmlElements.join(" "));
 };
 
+const manageSpinner=(status)=>{
+    if(status == true){
+        document.getElementById("spinner").classList.remove("hidden")
+        document.getElementById("word-container").classList.add("hidden")
+    }else {
+        document.getElementById("word-container").classList.remove("hidden")
+        document.getElementById("spinner").classList.add("hidden")
+    }
+}
+
 const loadLessons = () => {
     fetch("https://openapi.programming-hero.com/api/levels/all") // promise of response
     .then(res=> res.json()) // promise of json data
@@ -15,6 +25,7 @@ const removeActive = () => {
 }
 
 const loadLevelWord=(id)=>{
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
     fetch(url)
     .then((res) => res.json())
@@ -75,6 +86,7 @@ const displayLevelWord = (words) => {
             <h2 class="font-bold text-3xl">নেক্সট Lesson এ যান</h2>
         </div>
         `;
+        manageSpinner(false);
         return;
     }
 
@@ -93,6 +105,7 @@ const displayLevelWord = (words) => {
         `;
         wordContainer.append(card);
     });
+    manageSpinner(false);
 }
 
 const displayLesson = (lessons) => {
